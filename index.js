aaaf89a7ed85ded951a49a7dbf143c6107542e57
Carlos Placencia <carlosplacencia2012@gmail.com>
2021-09-20 22:30:15 -0500

    testing

--- index.js
+++ index.js
@@ -1,124 +1,101 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-const morgan = require('morgan');
 const cors = require('cors')
 
-app.use(express.json())
+// Takes to JSON data from the request and converts it to a javascript 
+// object and then attaches it to the body property
+app.use(express.json());
+
+// Allows request from all origin (CORS Error)
 app.use(cors())
 
-// morgan.token('data', function (req, res) {return JSON.stringify(req.body)});
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms {:data}'));
+// Allows express to show static content
+// app.use(express.static('build'))
 
-let persons = [
+// You can use Morgan to log evry request made to server.
+
+let notes = [
     {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
+        id: 1,
+        content: "HTML is easy",
+        date: "2019-05-30T17:30:31.098Z",
+        important: true
     },
     {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2019-05-30T18:39:34.091Z",
+        important: false
     },
     {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2019-05-30T19:20:14.298Z",
+        important: true
     }
 ]
 
-app.get('/', (req, res) => {
-    res.json({content: "System is back online"})
+// get all notes
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
 });
 
-app.get('/info', (req, res) => {
-    const phonebookSize = persons.length;
+// get one note
+app.get('/api/notes/:id', (req, res) => {
+    let noteId = Number(req.params.id);
 
-    const months = {
-        1: "Jan",
-        2: "Feb",
-        3: "Mar",
-        4: "Apr",
-        5: "May",
-        6: "Jun",
-        7: "Jul",
-        8: "Aug",
-        9: "Sep",
-        10: "Oct",
-        11: "Nov",
-        12: "Dec"
+    // Check if that note is in the array - return note
+    // If not in the array - return status code 400 {"Note not found"}
+    const note = notes.find(note => note.id === noteId);
+    if (!note) {
+        res.status(400).json({ error: "System malfunction, note cannot be found..." })
+    }
+    res.json(note);
+});
+
+// delete one note
+app.delete('/api/notes/:id', (req, res) => {
+    let noteId = Number(req.params.id);
+
+    // Check if noteID is in the array
+    // True - send back status: 204 
+    // False = send back status: 404 - {"System Malfunction, can't delete what is not there..."}
+    const toBeDeleted = notes.find(note => note.id === noteId);
+    if (!toBeDeleted) {
+        res.status(404).json({ error: "System malfunction, can't delete what is not there..." });
     }
 
-    const weekdays = {
-        0: "Sun",
-        1: "Mon",
-        2: "Tue",
-        3: "Wed",
-        4: "Thu",
-        5: "Fri",
-        6: "Sat"
-    }
-
-
-    var today = new Date();
-
-    const month = months[(today.getMonth() + 1)];
-    const weekday = weekdays[today.getDay()];
-    const date = today.getDate()
-    const year = today.getFullYear()
-    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    const recorderData = `${weekday} ${month} ${date} ${year} ${time} ${timezone}`;
-    const htmlInfo = `<div><p>Phone has info for ${phonebookSize} people</p><p>${recorderData}</p></div>`;
-
-    res.send(htmlInfo);
-})
-
-app.get('/api/persons', (req, res) => {
-    res.json(persons);
-})
-
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const person = persons.find(person => person.id === id);
-
-    res.json(person);
-})
-
-app.delete('/api/persons/:id', (req, res) => {
-    const delete_id = Number(req.params.id);
-    persons = persons.filter(person => person.id !== delete_id);
-
-    // Check if the item was actually deleted
+    // Actually remove the item from the array
     res.status(204).end();
-})
-
-app.post('/api/persons', (req, res) => {
-    const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id)) 
-    : 0
-
-    let newPerson = req.body;
-    newPerson.id = maxId + 1;
-
-    if(!newPerson.name || !newPerson.number){
-        return res.status(400).json({error: 'name or number missing'})
-    }
-
-    let personExists = persons.find((person => person.name === newPerson.name));
-    if(personExists){
-        return res.status(400).json({error: 'name must be unique'})
-    }
-
-    persons.push(newPerson);
-    res.json(newPerson);
 });
+
+//post one note
+app.post('/api/notes', (req, res) => {
+    const body = req.body;
+    if (!body.content) {
+        return res.status(400).json({ error: "System malfunction, conten is misisng..." });
+    }
+
+    const note = {
+        id: randomId(),
+        content: body.content,
+        date: new Date(),
+        important: body.important || false,
+    }
+
+    notes = notes.concat(note);
+    res.json(note);
+});
+
+
+const randomId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+
+    return maxId + 1;
+}
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
