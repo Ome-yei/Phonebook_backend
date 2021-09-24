bd039d111eb0e14fe2bfa9c386570e21e87dcb0a
Carlos Placencia <carlosplacencia2012@gmail.com>
2021-09-24 11:23:31 -0500

    adding front-end

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(express.json());
 // Allows request from all origin (CORS Error)
 app.use(cors())
 
+app.use(express.static('build'))
+
 // morgan.token('data', function (req, res) {return JSON.stringify(req.body)});
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms {:data}'));
 
